9e38fd75aa7ec5995876e4db534a339b5d7221a3
Igor Shevtsov <infoshevtsov@gmail.com>
2024-03-11 10:33:47 +0000

    finish with structs

---
+++ structs/v4/shapes.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+// Rectangle has the dimensions of a rectangle.
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+// Perimeter returns the perimeter of a rectangle.
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
