c0743616a33b16f204697a1a0519c07bda34ac81
Igor Shevtsov <infoshevtsov@gmail.com>
2024-03-14 11:16:16 +0000

    finish with maps

---
+++ maps/v7/dictionary.go
@@ -0,0 +1,63 @@
+package main
+
+// ErrNotFound means the definition could not be found for the given word.
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
+// Dictionary store definitions to words.
+type Dictionary map[string]string
+
+// Search find a word in the dictionary.
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
+}
+
+// Add inserts a word and definition into the dictionary.
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+	return nil
+}
+
+// Update changes the definition of a given word.
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExist
+	case nil:
+		d[word] = definition
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Delete removes a word from the dictionary.
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
