5e80b0ae90ca1d6a8ba8036d8d17796ecc51a948
Igor Shevtsov <infoshevtsov@gmail.com>
2024-04-06 20:21:57 +0100

    finish with Reading files

---
+++ reading-files/blogposts_test.go
@@ -0,0 +1,62 @@
+package blogposts_test
+
+import (
+	blogposts "learn_go_with_tests/reading-files"
+	"reflect"
+	"testing"
+	"testing/fstest"
+)
+
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+___
+A
+L
+M`
+	)
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assertPostsLength(t, posts, fs)
+
+	assertPost(t, posts[0], blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertPostsLength(t *testing.T, posts []blogposts.Post, fs fstest.MapFS) {
+	t.Helper()
+
+	if len(posts) != len(fs) {
+		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	}
+
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
+}
