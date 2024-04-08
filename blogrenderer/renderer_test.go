dbd962def6c5b141611a322cc07ae1ad4aa1c43e
Igor Shevtsov <infoshevtsov@gmail.com>
2024-04-08 10:35:40 +0100

    add approval test for templating

---
+++ blogrenderer/renderer_test.go
@@ -0,0 +1,30 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"learn_go_with_tests/blogrenderer"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module learn_go_with_tests
 
 go 1.22
+
+require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
